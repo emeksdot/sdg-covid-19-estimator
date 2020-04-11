debd4c04fedcc480c836262b52006b4377ab6cf3
emeksdot <emeksdot@yahoo.com>
2020-04-11 13:45:19 +0100

    Challenge 1 completion.

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,44 @@
-const covid19ImpactEstimator = (data) => data;
+
+const covid19ImpactEstimator = (data) => {
+  const {
+    region, periodType, timeToElapse, reportedCases, population, totalHospitalBeds
+  } = data;
+  const impactCurrentlyInfected = reportedCases * 10;
+  const severeImpactCurrentlyInfected = reportedCases * 50;
+
+  const impactEstimateOfInfected = (periodType)=>{
+    if (periodType === "days"){
+      return (impactCurrentlyInfected * 2 ** Math.floor(timeToElapse/3));
+    } else if (periodType === "weeks"){
+      return (impactCurrentlyInfected * 2 ** Math.floor(timeToElapse * 30/3));
+    } else if (periodType === "months"){
+      return (impactCurrentlyInfected * 2 ** Math.floor(timeToElapse * 30/3));
+    } else {
+      return "Invalid time period."
+    }
+  };
+
+  const severeEstimateOfInfected = (periodType)=>{
+    if (periodType === "days"){
+      return (severeImpactCurrentlyInfected * 2 ** Math.floor(timeToElapse/3));
+    } else if (periodType === "weeks"){
+      return (severeImpactCurrentlyInfected * 2 ** Math.floor(timeToElapse * 30/3));
+    } else if (periodType === "months"){
+      return (severeImpactCurrentlyInfected * 2 ** Math.floor(timeToElapse * 30/3));
+    } else {
+      return "Invalid time period."
+    }
+  };
+
+  const impact = {};
+  impact.currentlyInfected = impactCurrentlyInfected;
+  impact.infectionsByRequestedTime = impactEstimateOfInfected(periodType);
+  const severeImpact = {};
+  severeImpact.currentlyInfected = severeImpactCurrentlyInfected;
+  severeImpact.infectionsByRequestedTime = severeEstimateOfInfected(periodType);
+
+  return ({ data, impact, severeImpact });
+};
+
 
 export default covid19ImpactEstimator;
